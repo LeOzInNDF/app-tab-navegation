f1ce599dd2dbb781f78e4eef31a571fc6ab30262
LeOzInNDF <leo_albano@hotmail.com>
2024-09-19 22:02:49 -0300

    code-update

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Entypo, FontAwesome} from '@expo/vector-icons';
 
-export default function App() {
+import TelaInicial from "./componentes/TelaInicial";
+import TelaSobre from "./componentes/TelaSobre";
+
+const Tabs = createBottomTabNavigator();
+
+export default function App () {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    <NavigationContainer >
+      <Tabs.Navigator>
+        <Tabs.Screen name="Início" component = { TelaInicial } options={{tabBarIcon: ({color}) => (<Entypo name="home" color={ color } size={24} />),}}/>
+        <Tabs.Screen name="Sobre" component = { TelaSobre }  options={{
+            tabBarIcon: ({ color }) => (
+              <FontAwesome name="question-circle-o" color={ color } size={ 24 } />
+            ),
+          }} />
+      </Tabs.Navigator>
+      <StatusBar style='auto'/>
+    </NavigationContainer>
+  )
+};
---
+++ componentes/TelaInicial/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function TelaInicial(){
+  return (
+    <View>
+      <Text>TelaInicial</Text>
+    </View>
+  )
+}
